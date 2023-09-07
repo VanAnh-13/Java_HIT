58408c2733835bcb16b61f6da66b7ad66ac273a0
VanAnh-13 <140631235+VanAnh-13@users.noreply.github.com>
2023-09-07 17:39:51 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,17 @@
-package bai3;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+package bai1;
 
 public class Main {
+
 	public static void main(String[] args) {
-		Scanner in = new Scanner (System.in);
+		Cat cat = new Cat();
+		cat.input();
+		cat.output();
+		cat.tiengKeu();
 		
-		System.out.println("Nhap tong so hang hoa gia dung : ");
-		int giaDung = in.nextInt();
-		
-		ArrayList<HangHoaGiaDung> hangHoagiaDung = new ArrayList<>();
-		
-		for(int i = 0; i < giaDung; i++) {
-			System.out.println("Nhap thong tin hang hoa gia dung thu " + ( i + 1) + " : ");
-			
-			HangHoaGiaDung hanghoa = new HangHoaGiaDung();
-			hanghoa.input();
-			hangHoagiaDung.add(hanghoa);
-		}
-		
-		for(int i = 0; i < giaDung; i++) {
-			System.out.println("Thong tin hang hoa gia dung thu " + ( i + 1 ) + " la : ");
-			hangHoagiaDung.get(i).output();
-		}
-		
-		System.out.println("Nhap tong so hang hoa dien tu : ");
-		int dienTu = in.nextInt();
-		
-		ArrayList<HangHoaDienTu> hangHoadienTu = new ArrayList<>();
-		
-		for(int i = 0; i < dienTu; i++) {
-			System.out.println("Nhap thong tin hang hoa dien tu thu " + ( i + 1) + " : ");
-			
-			HangHoaDienTu hanghoa = new HangHoaDienTu();
-			hanghoa.input();
-			hangHoadienTu.add(hanghoa);
-		}
-		
-		for(int i = 0; i < giaDung; i++) {
-			System.out.println("Thong tin hang hoa dien tu thu " + ( i + 1 ) + " la : ");
-			hangHoadienTu.get(i).output();
-		}
+		Dog dog = new Dog();
+		dog.input();
+		dog.output();
+		dog.tiengKeu();
 	}
+
 }
